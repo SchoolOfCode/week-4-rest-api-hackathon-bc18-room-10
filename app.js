be3ba1fe1e12151da10505fe69d5509a2acd232a
pepland <136370802+pepland@users.noreply.github.com>
2024-12-19 13:40:53 +0000

    Started our project with important files

---
+++ app.js
@@ -0,0 +1,17 @@
+// Import the express module which is a web framework for Node.js
+import express from "express";
+
+// Create an instance of an Express application
+const app = express();
+
+// Middleware to parse incoming JSON requests and make it available under req.body
+app.use(express.json());
+
+// Export the app instance so it can be used in other files
+export default app;
+
+const PORT = 3000;
+
+app.listen(PORT, function () {
+  console.log(`Server is now listening on http://localhost:${PORT}`);
+});
