534bfc1f55e9a0247c1d92ae7d96ffa92923b184
pepland <136370802+pepland@users.noreply.github.com>
2024-12-20 00:30:46 +0000

    Issue #9 to fix

--- app.js
+++ app.js
@@ -16,32 +16,41 @@ app.listen(PORT, function () {
   console.log(`Server is now listening on http://localhost:${PORT}`);
 });
 
-import {
-    getMovies,
-    getMovieById
-} from "./helpers.js"
+import { getMovies, getMovieById, updateMovieById } from "./helpers.js";
 
 //get request for all movies
 app.get("/movies", async function (req, res) {
-    const returnedData = await getMovies();
-    const allMovies = {
-        "success": true,
-        "payload": returnedData,
-    }
-    res.json(allMovies);
-})
+  const returnedData = await getMovies();
+  const allMovies = {
+    success: true,
+    payload: returnedData,
+  };
+  res.json(allMovies);
+});
 
 //get request for a particular movie
 //get request with path as /movies:id
-app.get("/movies/:id", async function (req,res){
-    //delcare the id and make sure we change the id to a number
-    //use the getMoviebyId function with one parameter
-    const returnedData = await getMovieById(Number(req.params.id))
-    //create a success/payload object to store the function
-    const movieByID = {
-        "success": true,
-        "payload": returnedData,
-    }
-    //respond to the client with the specific movie
-    res.json(movieByID);
-})
+app.get("/movies/:id", async function (req, res) {
+  //delcare the id and make sure we change the id to a number
+  //use the getMoviebyId function with one parameter
+  const returnedData = await getMovieById(Number(req.params.id));
+  //create a success/payload object to store the function
+  const movieByID = {
+    success: true,
+    payload: returnedData,
+  };
+  //respond to the client with the specific movie
+  res.json(movieByID);
+});
+
+//Patch request to update a movie with a specific id
+app.patch("movies/:id", async function (req, res) {
+  //Store the updated data in a variable
+  const updatedData = await updateMovieById(Number(req.params.id), req.body);
+  //Store the updated movie with JSend specification
+  const updatedMovie = {
+    success: true,
+    payload: updatedData,
+  };
+  res.json(updatedMovie);
+});
--- helpers.js
+++ helpers.js
@@ -5,15 +5,36 @@ let movies = data.map((movie) => ({ ...movie, id: ++movieId }));
 
 //Create a function to get all movies
 export async function getMovies() {
-    return [...movies]; //... spreads out all of the elements/take everything in the array
-  }
+  return [...movies]; //... spreads out all of the elements/take everything in the array
+}
 
 //Create a function to get a particular movie
 //define a function that takes in one parameter(the movieId)
 export async function getMovieById(movieId) {
-//search through the movie array and return the first element with the matching id
-  return movies.find(function(movie) {
-//access the id of the current movie object being processed by .find, check if it is equal to the movieId
-  return movie.id === movieId;
+  //search through the movie array and return the first element with the matching id
+  return movies.find(function (movie) {
+    //access the id of the current movie object being processed by .find, check if it is equal to the movieId
+    return movie.id === movieId;
   });
+}
+
+//Support function to find the movie index within the array
+function findMovieIndexById(movieId) {
+  return movies.findIndex(({ id }) => id === movieId);
+}
+
+//Function we will use for the request with id of the interested movie and updates to be made
+export async function updateMovieById(movieId, updates) {
+  const index = findMovieIndexById(movieId);
+  //Handle the error in case of wrong id and avoid the system crash
+  if (index === -1) {
+    return;
+  }
+  //Update the object
+  const oldMovie = movies[index];
+  const updated = { ...oldMovie, ...updates, id: movieId };
+
+  movies = [...movies.slice(0, index), updated, ...movies.slice(index + 1)];
+  //Return the updated movie
+  return updated;
 }
